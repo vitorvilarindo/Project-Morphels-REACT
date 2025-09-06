46f774c4644e317d2a3ddcd1dedac31c954e1781
Vitor L Vilarindo <97807732+vitorvilarindo@users.noreply.github.com>
2025-09-06 07:47:57 -0300

    Update create_table.js

--- create_table.js
+++ create_table.js
@@ -5,11 +5,50 @@ import { sql } from './db.js'
 // })
 
 sql`
-CREATE TABLE IF NOT EXISTS videos (
+CREATE TABLE IF NOT EXISTS users (
   id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  title       VARCHAR(255) NOT NULL,
-  descripition TEXT,
-  duration    INTEGER NOT NULL
+  name        VARCHAR(200) NOT NULL,
+  email        VARCHAR(100) NOT NULL UNIQUE,
+  password   INTEGER NOT NULL
+  );
+`.then(() => {
+  console.log('tabela criada com sucesso  ')
+})
+
+sql`
+CREATE TABLE IF NOT EXISTS revenues (
+  id                UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  member            VARCHAR(200) NOT NULL,
+  type              VARCHAR(50) NOT NULL,
+  payment           VARCHAR(50) NOT NULL,
+  reference_mounth  VARCHAR(7) NOT NULL,
+  date              TIMESTAMP NOT NULL,
+  user_id           UUID REFERENCES users(id)
+  );
+`.then(() => {
+  console.log('tabela criada com sucesso  ')
+})
+
+sql`
+CREATE TABLE IF NOT EXISTS expenses (
+  id                    UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  title                 VARCHAR(200) NOT NULL,
+  category              VARCHAR(50) NOT NULL,
+  payment               VARCHAR(50) NOT NULL,
+  reference_mounth      VARCHAR(7) NOT NULL,
+  date                  TIMESTAMP NOT NULL,
+  beneficiary           VARCHAR(200) NOT NULL,
+  user_id               UUID REFERENCES users(id)
+  );
+`.then(() => {
+  console.log('tabela criada com sucesso  ')
+})
+
+sql`
+CREATE TABLE IF NOT EXISTS members (
+  id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name        VARCHAR(200) NOT NULL,
+  user_id     UUID REFERENCES users(id)
   );
 `.then(() => {
   console.log('tabela criada com sucesso  ')
