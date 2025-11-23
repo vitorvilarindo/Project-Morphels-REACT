9fe664411b097e3308501f7a55760298e2fc2041
Vitor L Vilarindo <97807732+vitorvilarindo@users.noreply.github.com>
2025-11-23 13:25:45 -0300

    Add CRUD operations for companies entity
    Introduced the dataBasePostgresCompanies class with methods for creating, listing, editing, and deleting companies in the database. Added corresponding Fastify routes in server.js to handle company-related API requests.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import { fastify } from 'fastify'
-import {  dataBasePostgresUsers, dataBasePostgresRevenues, dataBasePostgresExpenses, dataBasePostgresMembers } from './database_postgres.js'
+import {  dataBasePostgresUsers, dataBasePostgresRevenues, dataBasePostgresExpenses, dataBasePostgresMembers, dataBasePostgresCompanies } from './database_postgres.js'
 import cors from '@fastify/cors'
 
 const server = fastify()
@@ -11,6 +11,7 @@ const database_users = new dataBasePostgresUsers()
 const database_revenues = new dataBasePostgresRevenues()
 const database_expenses = new dataBasePostgresExpenses()
 const database_members = new dataBasePostgresMembers()
+const database_companies = new dataBasePostgresCompanies()
 
 //USERS
 server.post('/users', async (request, reply) => {
@@ -136,6 +137,7 @@ server.delete('/expenses/:id', async (request, reply) => {
 })
 
 //MEMBERS
+
 server.post('/members', async (request, reply) => {
   const { name, cellphone, date_birth, pixkey, pixtype } = request.body
   await database_members.create_members({
@@ -149,11 +151,13 @@ server.post('/members', async (request, reply) => {
   reply.status(201).send()
   console.log("Deu bom")
 })
+
 server.get('/members', async (request, reply) => {
   const search = request.query.search
   const members = await database_members.list_members(search)
   return reply.status(200).send(members)
 })
+
 server.put('/members/:id', async (request, reply) => {
   const memberID = request.params.id
   const { name, user_id } = request.body
@@ -163,11 +167,82 @@ server.put('/members/:id', async (request, reply) => {
   })
   return reply.status(204).send()
 })
+
 server.delete('/members/:id', async (request, reply) => {
   const memberID = request.params.id
   await database_members.delete_members(memberID)
   return reply.status(204).send()
 })
+
 server.listen(
   { port: 3000}
 )
+
+// COMPANIES
+
+server.post('/companies', async (request, reply) => {
+  const { CNPJ, company_name, fantasy_name, estate_registration, municipal_registration, open_date, situation, cep, street, number, complement, neighborhood, city, UF, cellphone, email, CNAE, activity_description, pixkey, pixtype } = request.body
+  await database_companies.create_company({
+    CNPJ,
+    company_name,
+    fantasy_name,
+    estate_registration,
+    municipal_registration,
+    open_date,
+    situation,
+    cep,
+    street,
+    number,
+    complement,
+    neighborhood,
+    city,
+    UF,
+    cellphone,
+    email,
+    CNAE,
+    activity_description,
+    pixkey,
+    pixtype,
+    user_id: "422a0acd-0210-4723-9622-c2b554ee8d60"
+  })
+  reply.status(201).send()
+  console.log("Deu bom")
+})
+server.get('/companies', async (request, reply) => {
+  const search = request.query.search
+  const companies = await database_companies.list_companies(search)
+  return reply.status(200).send(companies)
+})
+server.put('/companies/:id', async (request, reply) => {
+  const companyID = request.params.id
+  const { CNPJ, company_name, fantasy_name, estate_registration, municipal_registration, open_date, situation, cep, street, number, complement, neighborhood, city, UF, cellphone, email, CNAE, activity_description, pixkey, pixtype, user_id } = request.body
+  await database_companies.edit_companies(companyID, {
+    CNPJ,
+    company_name,
+    fantasy_name,
+    estate_registration,
+    municipal_registration,
+    open_date,
+    situation,
+    cep,
+    street,
+    number,
+    complement,
+    neighborhood,
+    city,
+    UF,
+    cellphone,
+    email,
+    CNAE,
+    activity_description,
+    pixkey,
+    pixtype,
+    user_id
+  })
+  return reply.status(204).send()
+})
+server.delete('/companies/:id', async (request, reply) => {
+  const companyID = request.params.id
+  await database_companies.delete_companies(companyID)
+  return reply.status(204).send()
+})
