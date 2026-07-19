dbd0c769c1d740dd8e4e21daa5738e978dada96a
Vitor L Vilarindo <97807732+vitorvilarindo@users.noreply.github.com>
2026-07-19 10:31:34 -0300

    Convert db.js to ES module and streamline code
    Refactor db.js to use ES module syntax and simplify export.

--- db.js
+++ db.js
@@ -1,15 +1,4 @@
-const { neon } = require('@neondatabase/serverless');
+import "dotenv/config";
+import { neon } from "@neondatabase/serverless";
 
-// O Render injeta a variável DATABASE_URL automaticamente 
-// se você configurou o banco no painel deles ou adicionou em Environment Variables.
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  console.error("❌ ERRO: A variável de ambiente DATABASE_URL não está definida!");
-}
-
-// Inicializa o cliente do Neon
-const sql = neon(connectionString);
-
-// Exporta o 'sql' diretamente para ser usado nos seus Services
-module.exports = sql;
+export const sql = neon(process.env.DATABASE_URL);
