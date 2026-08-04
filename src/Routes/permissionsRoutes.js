dc1f7a4dd5eb6310afe4b8d2b71ffc08217fb9b2
Vitor L Vilarindo <97807732+vitorvilarindo@users.noreply.github.com>
2026-08-04 08:57:41 -0300

    Delete src/Routes directory

--- src/Routes/permissionsRoutes.js
+++
@@ -1,5 +0,0 @@
-export default async function permissionsRoutes(server) {
-    const permissionsController = server.controllers.permissions
-
-    server.get("/permissions",permissionsController.listViewPermissions)
-}
